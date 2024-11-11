e6f04865227cc900b808d0cc0667852fc577bc0f
Salma Mamdoh <52897508+Salma-Mamdoh@users.noreply.github.com>
2024-11-11 20:52:28 +0200

    last version

--- src/main/java/com/studentApp/util/XMLUtil.java
+++ src/main/java/com/studentApp/util/XMLUtil.java
@@ -16,29 +16,57 @@ import java.util.List;
 public class XMLUtil {
 
     // Create a new empty XML file if it does not exist
-    public static void createNewXMLFile(String filePath) throws Exception {
+    public static void createNewXMLFile(String filePath) throws ParserConfigurationException, TransformerException, IOException {
         File file = new File(filePath);
-        if (!file.exists() && file.createNewFile()) {
-            System.out.println("XML file created at: " + file.getAbsolutePath());
+        if (!file.exists()) {
+            boolean res = file.createNewFile(); // Ensure the file is created
+            if (res) {
+                System.out.println("XML file created at: " + file.getAbsolutePath());
+            }
         }
 
-        Document doc = createDocument();
+        // Create an empty XML structure with a root element
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        Element rootElement = doc.createElement("University");
+        doc.appendChild(rootElement);
+
+        // Write the empty document to the file
         writeDocumentToFile(doc, file);
     }
 
     // Read students from XML file
     public static University readStudentsFromXML(String filePath) throws Exception {
         File xmlFile = new File(filePath);
-        if (!xmlFile.exists()) throw new IOException("XML file not found: " + filePath);
+        if (!xmlFile.exists()) {
+            throw new IOException("XML file not found: " + filePath);
+        }
 
-        Document doc = parseDocument(xmlFile);
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize(); // Normalize the XML structure
+
         University university = new University();
         NodeList studentList = doc.getElementsByTagName("Student");
 
         for (int i = 0; i < studentList.getLength(); i++) {
-            Element studentElement = (Element) studentList.item(i);
-            Student student = parseStudent(studentElement);
-            university.addStudent(student);
+            Node studentNode = studentList.item(i);
+            if (studentNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element studentElement = (Element) studentNode;
+                String studentID = studentElement.getAttribute("ID");
+                String firstName = studentElement.getElementsByTagName("FirstName").item(0).getTextContent();
+                String lastName = studentElement.getElementsByTagName("LastName").item(0).getTextContent();
+                String gender = studentElement.getElementsByTagName("Gender").item(0).getTextContent();
+                double gpa = Double.parseDouble(studentElement.getElementsByTagName("GPA").item(0).getTextContent());
+                int level = Integer.parseInt(studentElement.getElementsByTagName("Level").item(0).getTextContent());
+                String address = studentElement.getElementsByTagName("Address").item(0).getTextContent();
+
+                Student student = new Student(studentID, firstName, lastName, gender, gpa, level, address);
+                university.addStudent(student);
+            }
         }
 
         return university;
@@ -46,71 +74,61 @@ public class XMLUtil {
 
     // Save the list of students to XML
     public static void saveStudentsToXML(String filePath, University university) throws Exception {
-        Document doc = createDocument();
+        File xmlFile = new File(filePath);
+
+        // Prepare document structure
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
         Element rootElement = doc.createElement("University");
         doc.appendChild(rootElement);
 
-        for (Student student : university.getStudents()) {
-            Element studentElement = createStudentElement(doc, student);
+        List<Student> students = university.getStudents();
+        for (Student student : students) {
+            Element studentElement = doc.createElement("Student");
+            studentElement.setAttribute("ID", student.getId());
+
+            Element firstName = doc.createElement("FirstName");
+            firstName.appendChild(doc.createTextNode(student.getFirstName()));
+            studentElement.appendChild(firstName);
+
+            Element lastName = doc.createElement("LastName");
+            lastName.appendChild(doc.createTextNode(student.getLastName()));
+            studentElement.appendChild(lastName);
+
+            Element gender = doc.createElement("Gender");
+            gender.appendChild(doc.createTextNode(student.getGender()));
+            studentElement.appendChild(gender);
+
+            Element gpa = doc.createElement("GPA");
+            gpa.appendChild(doc.createTextNode(String.valueOf(student.getGpa())));
+            studentElement.appendChild(gpa);
+
+            Element level = doc.createElement("Level");
+            level.appendChild(doc.createTextNode(String.valueOf(student.getLevel())));
+            studentElement.appendChild(level);
+
+            Element address = doc.createElement("Address");
+            address.appendChild(doc.createTextNode(student.getAddress()));
+            studentElement.appendChild(address);
+
             rootElement.appendChild(studentElement);
         }
 
-        writeDocumentToFile(doc, new File(filePath));
+        // Save the document to XML file with pretty print
+        writeDocumentToFile(doc, xmlFile);
     }
 
-    // Helper methods
-    private static Document createDocument() throws ParserConfigurationException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        return builder.newDocument();
-    }
-
-    private static Document parseDocument(File xmlFile) throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(xmlFile);
-        doc.getDocumentElement().normalize();
-        return doc;
-    }
-
-    private static Student parseStudent(Element studentElement) {
-        String studentID = studentElement.getAttribute("ID");
-        String firstName = getTextContent(studentElement, "FirstName");
-        String lastName = getTextContent(studentElement, "LastName");
-        String gender = getTextContent(studentElement, "Gender");
-        double gpa = Double.parseDouble(getTextContent(studentElement, "GPA"));
-        int level = Integer.parseInt(getTextContent(studentElement, "Level"));
-        String address = getTextContent(studentElement, "Address");
-        return new Student(studentID, firstName, lastName, gender, gpa, level, address);
-    }
-
-    private static String getTextContent(Element parent, String tagName) {
-        return parent.getElementsByTagName(tagName).item(0).getTextContent();
-    }
-
-    private static Element createStudentElement(Document doc, Student student) {
-        Element studentElement = doc.createElement("Student");
-        studentElement.setAttribute("ID", student.getId());
-        appendChildWithText(doc, studentElement, "FirstName", student.getFirstName());
-        appendChildWithText(doc, studentElement, "LastName", student.getLastName());
-        appendChildWithText(doc, studentElement, "Gender", student.getGender());
-        appendChildWithText(doc, studentElement, "GPA", String.valueOf(student.getGpa()));
-        appendChildWithText(doc, studentElement, "Level", String.valueOf(student.getLevel()));
-        appendChildWithText(doc, studentElement, "Address", student.getAddress());
-        return studentElement;
-    }
-
-    private static void appendChildWithText(Document doc, Element parent, String tagName, String text) {
-        Element child = doc.createElement(tagName);
-        child.appendChild(doc.createTextNode(text));
-        parent.appendChild(child);
-    }
-
+    // Helper method to write document to file with pretty print
     private static void writeDocumentToFile(Document doc, File file) throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
+
+        // Set properties for pretty printing
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2"); // Set indentation
+
         DOMSource source = new DOMSource(doc);
         StreamResult result = new StreamResult(file);
         transformer.transform(source, result);
